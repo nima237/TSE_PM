8ca1c35fcdff304d8fc1c99152f29491f05ae153
nima237 <nimaheidari72@gmail.com>
2025-12-23 09:33:11 +0330

    Fix vertical alignment of icons and text in info cards

--- setup_git.bat
+++ setup_git.bat
@@ -2,10 +2,11 @@
 chcp 65001 >nul
 echo Setting up Git configuration...
 git config --global user.email "nimaheidari72@gmail.com"
-git config --global user.name "Nima Heidari"
+git config --global user.name "nimha2377"
 echo.
 echo Git configuration set successfully!
 echo Email: nimaheidari72@gmail.com
+echo Username: nimha2377
 echo.
 pause
 
